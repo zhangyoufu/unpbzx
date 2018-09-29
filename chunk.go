1b290fc166c83b086bfa8d476fe4bd5969b17023
Youfu Zhang <zhangyoufu@gmail.com>
2018-09-30 01:03:07 +0800

    import

---
+++ chunk.go
@@ -0,0 +1,7 @@
+package unpbzx
+
+type _Chunk struct {
+    idx     int
+    meta    int
+    data    []byte
+}
---
+++ unpbzx/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "context"
+    "log"
+    "os"
+    "runtime"
+
+    "github.com/palantir/stacktrace"
+    "github.com/zhangyoufu/unpbzx"
+)
+
+func main() {
+    var src *os.File
+    switch len(os.Args) {
+    case 1:
+        src = os.Stdin
+    case 2:
+        var err error
+        src, err = os.Open(os.Args[1])
+        if err != nil {
+            log.Fatal("cannot open input file: %s", err)
+        }
+    default:
+        log.Fatal("invalid usage")
+    }
+
+    ctx := context.Background()
+    dst := os.Stdout
+    numWorker := runtime.NumCPU()
+
+    if err := unpbzx.Extract(ctx, src, dst, numWorker); err != nil {
+        log.Fatal(stacktrace.Propagate(err, "error while extracting"))
+    }
+}
